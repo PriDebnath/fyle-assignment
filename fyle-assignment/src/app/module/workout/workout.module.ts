5de45a9bc4b44c9472b7e7472c5ed4fb6a914488
Pritam <debnathpritam0802@gmail.com>
2024-07-20 15:30:53 +0530

    created workout component

--- fyle-assignment/src/app/module/workout/workout.module.ts
+++ fyle-assignment/src/app/module/workout/workout.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WorkoutComponent } from './components/workout/workout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WorkoutComponent
+  ],
   imports: [
     CommonModule
   ]
